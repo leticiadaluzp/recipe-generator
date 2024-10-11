9ecb329750491e39b22f2255392eadcfd831df82
leticiadaluzp <leticia.daluz.p@gmail.com>
2024-10-11 16:55:27 -0300

    implement preference model

---
+++ app/models/preference.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: preferences
+#
+#  id          :bigint           not null, primary key
+#  name        :string
+#  description :text
+#  restriction :boolean
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Preference < ApplicationRecord
+end
