c94eb2f4b376c4775e377835e55e99f920ccd388
leticiadaluzp <leticia.daluz.p@gmail.com>
2024-10-11 17:09:01 -0300

    implement preferences controller

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -5,4 +5,16 @@ class PreferencesController < ApplicationController
     @preferences = current_user.preferences
     @pagy, @records = pagy(@preferences)
   end
+
+  def create
+    @preference = PreferenceForm.new(args: preference_form_params)
+
+    if @preference.valid?
+      @preference.save!
+      redirect_to preferences_path, notice: t('views.preferences.create_success')
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
 end
