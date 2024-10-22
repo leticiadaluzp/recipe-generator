20e2d1b3b1653f3cd15b87c3668b0e8db675d6f3
leticiadaluzp <leticia.daluz.p@gmail.com>
2024-10-22 17:44:41 -0300

    Add validations in the recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -18,4 +18,9 @@
 #
 class Recipe < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :ingredients, presence: true
+  validates :user_id, presence: true
 end
