6a9702ca2940c92bd65dae28ebcf06b409dc29a8
leticiadaluzp <leticia.daluz.p@gmail.com>
2024-11-14 21:49:05 -0300

    Add test for show_preference view

---
+++ spec/features/preferences/show_preference_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Show preference', :js do
+  let(:user) { create(:user) }
+  let(:preference) { create(:preference, name: 'MyString', description: 'MyText', restriction: false) }
+ 
+  before do
+    sign_in user
+    visit preferences_path
+    show_first_preference
+  end
+
+  describe '#show' do
+    context 'when clicking on some preference' do
+      it 'show the data of the requested preference' do
+        within '#panel' do
+          expect(page).to have_content('Name')
+          expect(page).to have_content('Description')
+          expect(page).to have_content('Restriction')
+        end
+      end
+
+      it 'the data it\'s not editable' do
+        expect(page).to have_field('preference[name]', disabled: true)
+        expect(page).to have_field('preference[description]', disabled: true)
+        expect(page).to have_field('preference[restriction]', disabled: true)
+      end
+    end
+  end
+
+  def show_first_preference
+    # within all('tbody tr').first do
+    #   click_on 'Show'
+    # end
+  end
+end
